fc152aa7a46e153d16f655e4621d62de9abc6948
Ciro <ciroreed@gmail.com>
2017-06-01 18:46:31 +0200

    added $$error advice container

--- src/interface/IFakeMethodReplacement.ts
+++ src/interface/IFakeMethodReplacement.ts
@@ -3,5 +3,6 @@ import { IStackEntry } from "./IStackEntry"
 export interface IFakeMethodReplacement {
   (...args: any[]): any
   $$before: IStackEntry[],
-  $$after: IStackEntry[]
+  $$after: IStackEntry[],
+  $$error: IStackEntry
 }
