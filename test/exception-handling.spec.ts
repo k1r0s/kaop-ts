ec5899f95855bef03dac65b6c1cbe49bd4430257
k1r0s <ciroreed@gmail.com>
2017-12-08 17:17:06 +0100

    revert: lint

--- test/exception-handling.spec.ts
+++ test/exception-handling.spec.ts
@@ -5,20 +5,20 @@ const catchVoid = onException(meta => meta.handle())
 class Person {
 
   @catchVoid
-  sayHello() {
-    throw new Error("sayHello err");
+  sayHello () {
+    throw new Error("sayHello err")
   }
 }
 
-let personInstance;
+let personInstance
 
 describe("exception handling advice", () => {
   beforeEach(() => {
-    personInstance = new Person;
+    personInstance = new Person()
   })
 
   it("Check error throw on invocation", () => {
-    personInstance.sayHello();
-  });
+    personInstance.sayHello()
+  })
 
 })
