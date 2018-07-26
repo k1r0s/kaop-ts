b065c7b2c3ef3dd2a9477655208ace2061850758
Albert Alises <albert.alises@gmail.com>
2018-07-26 12:21:35 +0200

    Modified onException decorator to be a function

--- src/on-exception.ts
+++ src/on-exception.ts
@@ -1,4 +1,6 @@
 import { afterMethod } from "./decorators"
 import { MethodSignature, AdviceRef } from "./interfaces"
 
-export const onException = <B = any, K extends keyof B = any>(advice: AdviceRef<B>): MethodSignature<B, K> => afterMethod(meta => meta.exception && advice(meta))
+export function onException<B = any, K extends keyof B = any> (advice: AdviceRef<B>): MethodSignature<B, K> {
+  return afterMethod(meta => meta.exception && advice(meta))
+}
